7f1549c34405d53e10f739aae06ca36577050450
haithemabderrahmen.belhadj@esprit.tn <haithemabderrahmen.belhadj@esprit.tn>
2018-11-02 18:39:06 +0100

    error

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
    .subscribe(next => {
      console.log('logged in successfuly');
    }, error => {
-     console.log('failed to loggin');
+     console.log(error);
    });
   }
 
