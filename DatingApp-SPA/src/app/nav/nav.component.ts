8d65e2f79ce01e6f9b3beb27e5d6455338721d0b
haithemabderrahmen.belhadj@esprit.tn <haithemabderrahmen.belhadj@esprit.tn>
2018-11-06 17:53:16 +0100

    endofpart6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +10,26 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
   login() {
    this.authService.login(this.model)
    .subscribe(next => {
-     console.log('logged in successfuly');
+     this.alertify.success('logged in successfuly');
    }, error => {
-     console.log(error);
+     this.alertify.error(error);
    });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+   const token = localStorage.getItem('token');
+   return !!token;
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
